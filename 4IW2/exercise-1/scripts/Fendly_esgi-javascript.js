bcece4118ae21bfa1f5af38c39b5a19175be662b
Karl MARQUES <kmarques@vetixy.com>
2020-10-20 17:48:05 +0200

    update reports

---
+++ 4IW2/exercise-1/scripts/Fendly_esgi-javascript.js
@@ -0,0 +1,34 @@
+function ucfirst(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.charAt(0).toUpperCase() + chaine.slice(1);
+}
+
+console.log(ucfirst("fendly"));
+
+function capitalize(chaine) {
+    return chaine
+        .toLowerCase()
+        .split(' ')
+        .map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        })
+        .join(' ');
+}
+console.log(capitalize("hello world"));
+
+
+function camelcase(chaine) {
+    chaine = capitalize(chaine);
+    return chaine.split(' ').join('');
+}
+
+console.log(camelcase("hello world slt"));
+
+function snakecase(chaine) {
+    return chaine.split(' ').join('_').toLowerCase();
+}
+
+console.log(snakecase("hello world slt"));
+
+module.exports.ucfirst = ucfirst;
+module.exports.capitalize = capitalize;
