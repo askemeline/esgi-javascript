9d6fc7dd78e2dc14747f408322256e098abf0762
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 16:23:30 +0200

    update reports

--- 4IW1/exercise-1/scripts/Djaizansi_esgi-javascript.js
+++ 4IW1/exercise-1/scripts/Djaizansi_esgi-javascript.js
@@ -8,7 +8,7 @@ function capitalize(chaine) {
     let str = "";
     let words = chaine.split(" ");
     for(let i=0;i<words.length;i++) {
-        if (i >= 0) {
+        if (i > 0) {
             str += " ";
         }
         str += ucfirst(words[i]);
@@ -21,8 +21,12 @@ function camelCase(chaine){
     return capitalize(chaine).split(' ').join('');
 }
 
-console.log(camelCase('coucou bien ou quoi'));
+function snake_case(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.split(' ').map(c => c.charAt(0).toUpperCase() + c.slice(1)).join('_').toLowerCase();
+}
 
 module.exports.ucfirst = ucfirst;
 module.exports.capitalize = capitalize;
 module.exports.camelCase = camelCase;
+module.exports.snake_case = snake_case;
