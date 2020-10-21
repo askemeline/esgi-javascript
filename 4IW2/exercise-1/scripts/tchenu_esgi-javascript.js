839e02b8a0a6d8274db51afa4c2917dc6bf7543d
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 09:06:26 +0200

    update reports

--- 4IW2/exercise-1/scripts/tchenu_esgi-javascript.js
+++ 4IW2/exercise-1/scripts/tchenu_esgi-javascript.js
@@ -37,17 +37,6 @@ function prop_access(object, path) {
     return result
 }
 
-function camelCase(string) {
-    const separators = [' ', '-', '_'];
-
-    separators.forEach(function (separator) {
-        string = string.replace(separator, '_')
-    })
-
-    return string.split('_').join('')
-}
-
 
 module.exports.ucfirst = ucfirst;
-module.exports.camelCase = camelCase;
 module.exports.prop_access = prop_access;
