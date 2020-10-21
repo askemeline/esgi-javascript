37b9871de043287a17acc280b07f4c111062eaf8
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 09:34:33 +0200

    update reports

--- 4IW2/exercise-1/scripts/Adeams1805_esgi-javascript.js
+++ 4IW2/exercise-1/scripts/Adeams1805_esgi-javascript.js
@@ -60,11 +60,7 @@ function leet(str) {
 }
 
 function prop_access(obj, path) {
-  if(typeof path !== "string" || path === "" || typeof obj !== "object") return obj;
-  if(obj === null) {
-    console.log(`${obj} not exist`);
-    return false;
-  }
+  if(typeof path !== "string" || path === "" || typeof obj !== "object" || obj === null) return obj;
   let args = path.split(".");
   let result = obj;
   let p = "";
@@ -77,6 +73,7 @@ function prop_access(obj, path) {
       return false;
     }
   }
+  if(result === undefined) return `${path} does not exist`;
   return result;
 }
 
--- 4IW2/exercise-1/string.js
+++ 4IW2/exercise-1/string.js
@@ -62,3 +62,30 @@ function yoda(chaine) {
 }
 
 console.log(yoda("bonjour la france"));
+
+function vig(string, code) {
+  if (typeof string !== "string") return "";
+  if (string.length === 0) return string;
+
+  while (code.length < string.length) {
+    code += code;
+  }
+  code = code.substr(0, string.length);
+  let codeIndex = 0;
+
+  return string
+    .split("")
+    .map((letter, index) => {
+      letter = letter.toLowerCase();
+      const aCode = "a".charCodeAt(0);
+      const letterNumber = letter.charCodeAt(0) - aCode; // [0-25]
+
+      if (letterNumber < 0 || letterNumber > 25) return letter;
+
+      const codeNumber = code.charCodeAt(codeIndex) - aCode; // [0-25]
+      codeIndex++;
+
+      return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+    })
+    .join("");
+}
