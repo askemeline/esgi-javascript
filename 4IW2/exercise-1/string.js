ceb50055c33a1dd7b43598f28a80cc5129e4ddc2
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 09:17:30 +0200

    update reports

--- 4IW2/exercise-1/string.js
+++ 4IW2/exercise-1/string.js
@@ -1,20 +1,14 @@
-function ucfirst(chaine) {​​​​​
-    if (typeof chaine !== "string" || chaine === "") return "";
-    return chaine.charAt(0).toUpperCase() + chaine.slice(1);
-}​​​​​
-
-
 function ucfirst(chaine) {
-    if(typeof chaine !== "string" || chaine === "") return "";
-    
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.charAt(0).toUpperCase() + chaine.slice(1);
 }
 function capitalize(chaine) {
-    if (typeof chaine !== "string" || chaine === "") return "";
+  if (typeof chaine !== "string" || chaine === "") return "";
 
-    return chaine
-        .split(" ")
-        .map(word => ucfirst(word.toLowerCase()))
-        .join(" ");
+  return chaine
+    .split(" ")
+    .map((word) => ucfirst(word.toLowerCase()))
+    .join(" ");
 }
 
 console.log(capitalize("bonjour LA FraNce"));
