5b8ec9baa01472c94c606e6f260df6bcfbb81377
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 17:30:21 +0200

    update reports

--- 4IW1/exercise-1/scripts/Djaizansi_esgi-javascript.js
+++ 4IW1/exercise-1/scripts/Djaizansi_esgi-javascript.js
@@ -1,25 +1,28 @@
+
 function ucfirst(chaine) {
-    if(typeof chaine !== "string" || chaine === "") return "";
+    if (typeof chaine !== "string" || chaine === "") return "";
     return chaine.charAt(0).toUpperCase() + chaine.slice(1);
 }
 
 function capitalize(chaine) {
-    if (typeof(chaine) != "string" || chaine === "") return "";
-    return chaine.split(' ').map(word => ucfirst(word.toLowerCase())).join(' ');
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine
+        .split(" ")
+        .map((word) => ucfirst(word.toLowerCase()))
+        .join(" ");
 }
 
-function camelCase(chaine){
+function camelCase(chaine) {
     if (typeof chaine !== "string" || chaine === "") return "";
     return capitalize(chaine).replace(/\W/g, "");
 }
 
 function snake_case(chaine) {
     if (typeof chaine !== "string" || chaine === "") return "";
-    return chaine.toLowerCase().replace(/\W/g, "");
+    return chaine.toLowerCase().replace(/\W/g, "_");
 }
 
 function leet(chaine) {
-    if (typeof chaine !== "string" || chaine === "") return "";
     return chaine.replace(/[aeiouy]/gi, function (e) {
         switch (e.toLowerCase()) {
             case "a":
@@ -38,14 +41,42 @@ function leet(chaine) {
     });
 }
 
-function verlan(chaine){
-    if (typeof chaine !== "string" || chaine === "") return "";
-    return chaine.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+function verlan(chaine) {
+    return chaine
+        .split(" ")
+        .map((word) => word.split("").reverse().join(""))
+        .join(" ");
 }
 
-function yoda(chaine){
-    if (typeof chaine !== "string" || chaine === "") return "";
+function yoda(chaine) {
     return chaine.split(" ").reverse().join(" ");
+}
+
+function vig(string, code) {
+    if (typeof string !== "string") return "";
+    if (string.length === 0) return string;
+
+    while (code.length < string.length) {
+        code += code;
+    }
+    code = code.substr(0, string.length);
+    let codeIndex = 0;
+
+    return string
+        .split("")
+        .map((letter, index) => {
+            letter = letter.toLowerCase();
+            const aCode = "a".charCodeAt(0);
+            const letterNumber = letter.charCodeAt(0) - aCode; // [0-25]
+
+            if (letterNumber < 0 || letterNumber > 25) return letter;
+
+            const codeNumber = code.charCodeAt(codeIndex) - aCode; // [0-25]
+            codeIndex++;
+
+            return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+        })
+        .join("");
 }
 
 module.exports.ucfirst = ucfirst;
@@ -55,3 +86,4 @@ module.exports.snake_case = snake_case;
 module.exports.leet = leet;
 module.exports.verlan = verlan;
 module.exports.yoda = yoda;
+module.exports.vig = vig;
