80f93b6b9d55619b3550ab8cfce4f388b66f7ab3
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 15:45:17 +0200

    update reports

---
+++ 4IW1/exercise-1/scripts/Djaizansi_esgi-javascript.js
@@ -0,0 +1,25 @@
+function ucfirst(chaine) {
+    if(typeof chaine !== "string" || chaine === "") return "";
+    return chaine.charAt(0).toUpperCase() + chaine.slice(1);
+}
+
+function capitalize(chaine) {
+    if (typeof(chaine) != "string" || chaine === "") return "";
+    let str = "";
+    let words = chaine.split(" ");
+    for(let i=0;i<words.length;i++) {
+        str += ucfirst(words[i]);
+    }
+    return str;
+}
+
+function camelCase(chaine){
+    if (typeof chaine !== "string" || chaine === "") return ""; 
+    return capitalize(chaine).split(' ').join('');
+}
+
+
+
+module.exports.ucfirst = ucfirst;
+module.exports.capitalize = capitalize;
+module.exports.camelCase = camelCase;
