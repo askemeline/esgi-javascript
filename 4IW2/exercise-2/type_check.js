b8a582526f7dd2b1f8f413341e0395e324fc0d23
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 18:58:06 +0200

    update reports

--- 4IW2/exercise-2/type_check.js
+++ 4IW2/exercise-2/type_check.js
@@ -25,5 +25,8 @@ console.log(type_check_v1({}, "null") === false);
 console.log(type_check_v1(undefined, "undefined") === true);
 
 
-function type_check_v2(variable, type) {
+function type_check_v2(variable, conf) {
+    for(toCheck in conf) {
+        case ''
+    }
 }
