811d8ddd94120415868147a45716ccfe07af4c4b
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 16:21:07 +0200

    update reports

---
+++ 4IW1/exercise-1/scripts/a-echeverria_esgi-javascript.js
@@ -0,0 +1,88 @@
+function ucfirst(string) {
+    return typeof string === "string" && string !== '' ? string[0].toUpperCase()+string.slice(1) : '';
+}
+
+function capitalize(string) {
+    let res = '';
+    if (typeof string !== "string" || string === '') {
+        return '';
+    }
+    const split = string.split(' ');
+    for (let i=0; i<split.length; i++) {
+        res += ucfirst(split[i])+' ';
+    }
+    return res.trim();
+}
+
+function camelCase(string) {
+    return capitalize(string).replaceAll(' ', '');
+}
+
+function snake_case(string) {
+    if (typeof string !== "string" || string === '') {
+        return '';
+    }
+    return string.toLowerCase().replace(/ /g, '_');
+}
+
+function leet(string) {
+    if (typeof string !== "string" || string === '') {
+        return '';
+    }
+    const letter = ["A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y"];
+    const crypt  = ["4", "3", "1", "0", "_", "7", "4", "3", "1", "0", "_", "7"];
+
+    let res = '';
+    for (let i = 0; i<string.length; i++) {
+        if (letter.includes(string[i])) {
+            res += crypt[letter.indexOf(string[i])];
+        } else res += string[i];
+    }
+    return res;
+}
+
+function verlan(string) {
+    if (typeof string !== "string" || string === '') {
+        return '';
+    }
+    let res = '';
+    const split = string.split(' ');
+    for (let word of split) {
+        for (let i = word.length; i > 0; i--) {
+            res += word[i-1]
+        }
+        res += ' ';
+    }
+    return res.trim();
+}
+
+function yoda(string) {
+    if (typeof string !== "string" || string === '') {
+        return '';
+    }
+    let res = '';
+    const split = string.split(' ');
+    for (let i = split.length; i > 0; i--) {
+        res += split[i-1] + ' ';
+    }
+    return res.trim();
+}
+
+function prop_access(object, path) {
+
+}
+
+function vig(string) {
+
+}
+
+
+module.exports.ucfirst = ucfirst;
+module.exports.capitalize = capitalize;
+module.exports.camelCase = camelCase;
+module.exports.snake_case = snake_case;
+module.exports.leet = leet;
+module.exports.verlan = verlan;
+module.exports.yoda = yoda;
+module.exports.vig = vig;
+module.exports.prop_access = prop_access;
