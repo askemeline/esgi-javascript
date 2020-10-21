1561865a6ea7da66d1ee1350391223e143230427
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 16:01:31 +0200

    update reports

---
+++ 4IW1/exercise-1/scripts/Theo-Ambrois_esgi-javascript.js
@@ -0,0 +1,36 @@
+function ucfirst(string){
+	if(typeof string !== "string" || string !== "")
+		return "";
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function capitalize(string) {
+    if (typeof string !== "string" && string !== "")
+    	return "";	
+
+    return string.split(" ").map((string) => {
+    	return ucfirst(string)
+    }
+    ).join(" ");
+}
+
+function camelCase(string) {
+    if (typeof string !== "string" && string !== "")
+    	return "";
+
+    return capitalize(string).split(" ").join("");
+}
+
+function snake_case(string) {
+    if (typeof string !== "string" && string !== "")
+    	return "";
+
+    string = string = string.toLowerCase();
+    return string.replace(" ", "-");
+
+}
+
+module.exports.ucfirst = ucfirst;
+module.exports.capitalize = capitalize;
+module.exports.camelCase = camelCase;
+module.exports.snake_case = snake_case;
