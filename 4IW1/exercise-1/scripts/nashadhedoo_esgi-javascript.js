4ecbc673df3f0930964adf2ee81f904ed409f823
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 15:49:22 +0200

    update reports

--- 4IW1/exercise-1/scripts/nashadhedoo_esgi-javascript.js
+++ 4IW1/exercise-1/scripts/nashadhedoo_esgi-javascript.js
@@ -1,6 +1,6 @@
 function ucfirst(chaine) {
 	if (typeof chaine !== "string" || chaine === "") return "";
-	return chaine.charAt(0).toUppercase() + chaine.slice(1);
+	return chaine.charAt(0).toUpperCase() + chaine.slice(1);
 }
 
 function capitalize(string) {
