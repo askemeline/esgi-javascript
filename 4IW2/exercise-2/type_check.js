a6ca182ac139f122fe322665912322fc627a9478
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 18:52:50 +0200

    update reports

--- 4IW2/exercise-2/type_check.js
+++ 4IW2/exercise-2/type_check.js
@@ -1,20 +1,20 @@
 function type_check_v1(variable, type) {
-    switch(typeof variable) {
-        case "symbol":
-        case "number":
-        case "string":
-        case "boolean":
-        case "undefined":
-        case "function":
-            return type === typeof variable;
-        case "object":
-            switch(type) {
-                case "null":
-                    return variable === null;
-                case "array":
-                    return Array.isArray(data);
-                default:
-                    return data !== null 
-            }
-    }
+  switch (typeof variable) {
+    case "symbol":
+    case "number":
+    case "string":
+    case "boolean":
+    case "undefined":
+    case "function":
+      return type === typeof variable;
+    case "object":
+      switch (type) {
+        case "null":
+          return variable === null;
+        case "array":
+          return Array.isArray(variable);
+        default:
+          return variable !== null && !Array.isArray(variable);
+      }
+  }
 }
