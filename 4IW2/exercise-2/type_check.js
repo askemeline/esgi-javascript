2b9e8069c4b67f829c491def8b618a648ff2eba1
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 19:02:24 +0200

    update reports

--- 4IW2/exercise-2/type_check.js
+++ 4IW2/exercise-2/type_check.js
@@ -35,8 +35,9 @@ function type_check_v2(variable, conf) {
           return false;
         break;
         case "enum":
+            let found = false;
             for(subValue of conf.enum) {
-                if(type_check_v2(variable, {value: subValue}))
+                if(!foundtype_check_v2(variable, {value: subValue}))
             }
             break;
     }
