99fe05c4c7d987dfaccd02d18189318847b99988
Karl MARQUES <kmarques@vetixy.com>
2020-10-21 18:34:03 +0200

    update reports

--- 4IW2/exercise-2/scripts/Lauffret_esgi_javascript.js
+++ 4IW2/exercise-2/scripts/Lauffret_esgi_javascript.js
@@ -4,9 +4,17 @@ function type_check_V1(arg1 , arg2){
 	}
 }
 
-function type_check_V2(arg1 , arg2[]){
-	if (typeof(agr1) == arg2){
-		return true;
+function type_check_V2(arg1 , arg2= null , arg3=null){
+	if (arg3 === ""){
+		if (typeof(agr1) == arg2){
+			return true;
+		}
 	}
+	if (arg2 === ""){
+		if (agr1 == arg2){
+			return true;
+		}
+	}
+	return false;
 }
 
