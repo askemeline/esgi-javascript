d3ce2e6d2d5b89f68cc251d275a0d0ef3f6fec53
Karl MARQUES <kmarques@vetixy.com>
2020-10-22 09:09:59 +0200

    update reports

--- 4IW2/exercise-2/scripts/aza93_esgi-javascript.js
+++ 4IW2/exercise-2/scripts/aza93_esgi-javascript.js
@@ -1,3 +1,23 @@
+/*
+function type_check_v1(v, t) {
+    if (typeof v === t)
+        return true;
+    else if (typeof v === "object") {
+        switch (t) {
+            case "null":
+                return v === null;
+            case "array":
+                return Array.isArray(v);
+            default:
+                return v !== null && !Array.isArray(v);
+        }
+    }
+
+
+    return false;
+}
+*/
+
 function type_check_v1(variable, type) {
   switch (typeof variable) {
     case "symbol":
@@ -19,11 +39,12 @@ function type_check_v1(variable, type) {
   }
 }
 
+
 //console.log(type_check_v1("sdfsdfdsfsdfd fdf", "number"));
 
 function type_check_v2(v, obj) {
   // if enum
-  if ("enum" in obj && obj["enum"].includes(v) && type_check_v1(v, "number")) {
+  if ("enum" in obj && obj["enum"].includes(v)) {
     return true;
   }
   else if ("type" in obj && !("value" in obj)) {
