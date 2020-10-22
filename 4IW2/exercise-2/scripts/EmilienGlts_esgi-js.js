26e15f86ff72ecc1e967a18daf6966384fff77f2
Karl MARQUES <kmarques@vetixy.com>
2020-10-22 08:55:23 +0200

    update reports

--- 4IW2/exercise-2/scripts/EmilienGlts_esgi-js.js
+++ 4IW2/exercise-2/scripts/EmilienGlts_esgi-js.js
@@ -1,23 +1,73 @@
-function type_check_v1(input, type) {
-    switch (typeof input) {
-        case "string":
-        case "symbol":
-        case "number":
-        case "function":
-        case "undefined":
-        case "boolean":
-            return typeof input === type;
-        case "object":
+function type_check_v1(val, type) {
+    switch (typeof val) {
+        case 'symbol':
+        case 'number':
+        case 'string':
+        case 'boolean':
+        case 'undefined':
+        case 'function':
+            return type === typeof val;
+        case 'object':
             switch (type) {
                 case 'null':
-                    return input === null;
+                    return val === null;
                 case 'array':
-                    return Array.isArray(input);
+                    return Array.isArray(val);
                 default:
-                    return input !== null && !Array.isArray(input);
+                    return val !== null && !Array.isArray(val);
             }
     }
 }
 
+function type_check_v2(val, conf) {
+    for (let check in conf) {
+        switch (check) {
+            case 'type':
+                if (type_check_v1(val, conf.type) === false) {
+                    return false;
+                }
+                break;
+            case 'value':
+                if (JSON.stringify(val) !== JSON.stringify(conf.value)) {
+                    return false;
+                }
+                break;
+            case 'enum':
+                let found = false;
+                for (let subValue of conf.enum) {
+                    if (!found) {
+                        found = type_check_v2(val, {value: subValue});
+                    } else {
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    return false;
+                }
+        }
+    }
+
+    return true;
+}
+
+function type_check(obj, conf) {
+    let check = type_check_v2(obj, conf);
+
+    if (!conf.properties) {
+        return check;
+    }
+
+    for (const typeKey in conf.properties) {
+        check = type_check(type_check_v1(obj, 'object') ? obj[typeKey] : obj, conf.properties[typeKey]);
+        if (!check) {
+            break
+        }
+    }
+
+    return check;
+}
 
 module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
+module.exports.type_check = type_check;
