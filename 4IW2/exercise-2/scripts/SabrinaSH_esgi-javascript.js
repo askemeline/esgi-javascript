001f4dcf0ae41702dbd16de5d6ea6519d9a6e108
Karl MARQUES <kmarques@vetixy.com>
2020-10-22 08:56:13 +0200

    update reports

--- 4IW2/exercise-2/scripts/SabrinaSH_esgi-javascript.js
+++ 4IW2/exercise-2/scripts/SabrinaSH_esgi-javascript.js
@@ -1,32 +1,62 @@
-function type_check_v1(value, type) {
-  if (
-    (value === undefined && type === "undefined") ||
-    (value === null && type === "object") ||
-    typeof value === type
-  ) {
-    return true;
+function type_check_v1(variable, type) {
+  switch (typeof variable) {
+    case "symbol":
+    case "number":
+    case "string":
+    case "boolean":
+    case "undefined":
+    case "function":
+      return type === typeof variable;
+    case "object":
+      switch (type) {
+        case "null":
+          return variable === null;
+        case "array":
+          return Array.isArray(variable);
+        default:
+          return variable !== null && !Array.isArray(variable);
+      }
   }
-  return false;
 }
 
-function type_check_v2(arg1, arg2) {
-  if (arg2 === null) {
-    return true;
-  }
-  if (arg2.type !== undefined) {
-    return type_check_v1(arg1, arg2.type);
-  }
-  if (arg2.value !== undefined) {
-    if (arg2.value === arg1) {
-      return true;
-    } else {
-      return false;
+console.log(type_check_v1(null, "null") === true);
+console.log(type_check_v1(null, "object") === false);
+console.log(type_check_v1({}, "null") === false);
+console.log(type_check_v1(undefined, "undefined") === true);
+
+function type_check_v2(variable, conf) {
+  for (toCheck in conf) {
+    switch (toCheck) {
+      case "type":
+        if (type_check_v1(variable, conf.type) === false) return false;
+        break;
+      case "value":
+        if (JSON.stringify(variable) !== JSON.stringify(conf.value))
+          return false;
+        break;
+      case "enum":
+        let found = false;
+        for (subValue of conf.enum) {
+          if (!found) found = type_check_v2(variable, { value: subValue });
+          if (found) break;
+        }
+        if (!found) return false;
+        break;
     }
   }
+  return true;
 }
 
-console.log(type_check_v1(1, "number"));
+function type_check(variable, conf) {
+  if (type_check_v2(variable, conf) === false) {
+    return false;
+  } else if (type_check_v1(conf.properties, conf.type) === false) {
+    return false;
+  }
+  return type_check(conf.properties, conf.type);
+}
 
 
 module.exports.type_check_v1 = type_check_v1;
 module.exports.type_check_v2 = type_check_v2;
+module.exports.type_check = type_check;
