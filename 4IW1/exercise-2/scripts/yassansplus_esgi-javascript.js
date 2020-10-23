270213cee97831d19608a4cb11e2cdd26afddb2b
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 17:55:30 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/yassansplus_esgi-javascript.js
@@ -0,0 +1,7 @@
+function type_check_v1(myNumber, typecheck ='number'){
+        return (typeof(myNumber) == typecheck);
+}
+
+console.log(type_check_v1("test"));
+
+module.exports.type_check_v1 = type_check_v1;
