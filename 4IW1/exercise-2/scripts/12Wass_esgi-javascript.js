7806e0a51d70ad0cf61bfd6aa5d3339ee95e76fa
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:28:52 +0200

    update reports

--- 4IW1/exercise-2/scripts/12Wass_esgi-javascript.js
+++ 4IW1/exercise-2/scripts/12Wass_esgi-javascript.js
@@ -1,13 +1,28 @@
 function type_check_v1(arg, type){
-    if (typeof(arg) !== type) {
-        return false;
+    if (typeof(arg) === "object"){
+        if (Array.isArray(arg)){
+            if (type === "array"){
+                return true;
+            }
+            return false; 
+        }
     }
-    return true; 
+        if (typeof(arg) !== type) {
+            return false;
+        }
+        return true; 
+ }
+
+function type_check_v2(arg, conf){
+    if (typeof(arg) == conf.type && arg == conf.value){
+        return true;
+    }
+    return false; 
 }
 
-function type_check_v2(){
-    
-}
+console.log(type_check_v1({"1": "1", "2": "2", "3": "3"}, "object")); // JSON 
+console.log(type_check_v1(["1", "2", "3"], "array")); // Array
+
 
 module.exports.type_check_v1 = type_check_v1;
 module.exports.type_check_v2 = type_check_v2;
