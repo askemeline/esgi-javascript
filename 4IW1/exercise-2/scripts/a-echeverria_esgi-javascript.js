d18ffa1dc0c01b7695de2b61d5083bf4903e3ff4
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:02:05 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/a-echeverria_esgi-javascript.js
@@ -0,0 +1,28 @@
+function type_check_v1(value, type) {
+    if (typeof value === "object") {
+        if (type === "array") {
+            return Array.isArray(value);
+        }
+        if (type === "null") {
+            return value === null;
+        }
+    }
+    return typeof value === type;
+}
+
+function type_check_v2(value, type) {
+    if (type["type"] && typeof value !== type["type"]) {
+        return false;
+    }
+    if (type["value"] && value !== type["value"]){
+        return false
+    }
+    if (type["enum"] && !(type["enum"].includes(value))) {
+        return false;
+    }
+    return true;
+}
+
+
+module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
