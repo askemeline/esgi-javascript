6bbcff701e935717d2fbf9833b218a3c9b59338b
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 19:04:05 +0200

    update reports

--- 4IW1/exercise-2/scripts/Djaizansi_esgi-javascript.js
+++ 4IW1/exercise-2/scripts/Djaizansi_esgi-javascript.js
@@ -1,10 +1,13 @@
 function type_check_v1(attr, type){
     if(typeof attr === 'object'){
-        if(attr === null) return true;
-        if(Array.isArray(attr)) return true;
-        if(attr !== null || !(Array.isArray(attr))) return true;
+        if(type === "null") return true;
+        if(type === "array") return Array.isArray(attr);
+        return (attr !== null || !(Array.isArray(attr)));
     }
     return (typeof attr === type);
 }
 
+function type_check_v2()
+
 module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
