9a9e3efe3377a4686bec5989665b9cbe77ca4eef
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 15:21:01 +0200

    update reports

---
+++ 4IW3/exercise-2/scripts/mlima95_esgi-javascript.js
@@ -0,0 +1,27 @@
+function type_check_v1(value,type){
+    if(typeof(value) !== "object"){
+        switch(typeof(value)){
+            case "function":
+            case "undefined":
+            case "boolean":
+            case "string":
+            case "number":
+            case "symbol":
+                return type === typeof(value); 
+        }
+    }else{
+        switch(type){
+            case "array":
+                console.log('is array');
+                return Array.isArray(value);
+            case "null":
+                return value === null;
+            default:
+                return !Array.isArray(value) && value !== null;
+        }
+    }
+        
+}
+
+
+module.exports.type_check_v1 = type_check_v1;
