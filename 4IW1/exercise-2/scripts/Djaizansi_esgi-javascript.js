b73ed99ff462950c8392265eb09779a0a6071388
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:03:40 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/Djaizansi_esgi-javascript.js
@@ -0,0 +1,5 @@
+function type_check_v1(attr, type){
+    return (typeof attr === type);
+}
+
+module.exports.type_check_v1 = type_check_v1;
--- 4IW1/exercise-2/scripts/taoberquer_cours-js.js
+++ 4IW1/exercise-2/scripts/taoberquer_cours-js.js
@@ -1,13 +1,15 @@
-function type_check_V1(value, type) {
+function type_check_v1(value, type) {
   return typeof value === type;
 }
 
-function type_check_V2(value, checkers) {
-  if (! 'type' in checkers || type_check_V1(value, checkers.type)) return false;
-  if (! 'value' in checkers || value != checkers.value) return false;
-  if (! 'enum' in checkers || ! checkers.enum.includes(value)) return false;
+function type_check_v2(value, checkers) {
+  if ('type' in checkers && ! type_check_V1(value, checkers.type)) return false;
+  if ('value' in checkers && value != checkers.value) return false;
+  if ('enum' in checkers && ! checkers.enum.includes(value)) return false;
 
   return true;
 }
 
 
+module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
