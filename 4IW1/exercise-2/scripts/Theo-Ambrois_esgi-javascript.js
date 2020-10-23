92ccc0d664ae775d1476c4b2ef8f63901696565e
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:15:55 +0200

    update reports

--- 4IW1/exercise-2/scripts/Theo-Ambrois_esgi-javascript.js
+++ 4IW1/exercise-2/scripts/Theo-Ambrois_esgi-javascript.js
@@ -1,5 +1,22 @@
-function type_check_v1(myNumber, typecheck ='number'){
-        return (typeof(myNumber) == typecheck);
+function type_check_v1(arg, type) {
+    switch (typeof arg) {
+        case "symbol":
+        case "number":
+        case "string":
+        case "boolean":
+        case "undefined":
+        case "function":
+            return type === typeof arg;
+        case "object":
+            switch (type) {
+                case "null":
+                    return arg === null;
+                case "array":
+                    return Array.isArray(arg);
+                default:
+                    return arg !== null && !Array.isArray(arg);
+            }
+    }
 }
 
 module.exports.type_check_v1 = type_check_v1;
