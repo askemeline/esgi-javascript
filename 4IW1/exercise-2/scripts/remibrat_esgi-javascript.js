50ffcec265ccdb105e792d0cedda3a0c98a0b34d
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 19:49:39 +0200

    update reports

--- 4IW1/exercise-2/scripts/remibrat_esgi-javascript.js
+++ 4IW1/exercise-2/scripts/remibrat_esgi-javascript.js
@@ -1,23 +1,50 @@
-function type_check_v1(arg1, arg2) {
-    if(Array.isArray(arg1)){
-        return arg2.toLowerCase() === "array";
+function type_check_v1(arg1, type) {
+    const typeOfArg = typeof arg1;
+
+    switch (typeOfArg) {
+        case 'object':
+            switch (type) {
+                case 'null':
+                    return arg1 === null;
+                case 'array':
+                    return Array.isArray(arg1);
+                case 'object':
+                    return arg1 !== null && !Array.isArray(arg1);
+                default:
+                    return false;
+            }
+        default:
+            return typeOfArg === type;
     }
-    return typeof arg1 === arg2.toLowerCase();
 }
 
 function type_check_v2(arg1, conf) {
-    if(conf["type"] !== undefined) {
-        if(typeof arg1 !== conf["type"]) return false;
-    }
-    if(conf["value"] !== undefined) {
-        if(arg1 !== conf["value"]) return false;
-    }
-    if(conf["enum"] !== undefined) {
-        if(!(arg1 in conf["enum"])) return false;
+    for(key in conf) {
+        switch (key) {
+            case 'type':
+                if (!type_check_v1(arg1, conf.type)) return false;
+                break;
+            case 'value':
+                if(JSON.stringify(arg1) !== JSON.stringify(conf.value)) return false
+                break;
+            case 'enum':
+                let found = false;
+                for(subValue in conf.enum){
+                    if(type_check_v2(arg1, {value: subValue})) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) return false;
+        }
     }
     return true;
 }
 
+function type_check() {
+
+}
 
 module.exports.type_check_v1 = type_check_v1;
 module.exports.type_check_v2 = type_check_v2;
+module.exports.type_check = type_check;
