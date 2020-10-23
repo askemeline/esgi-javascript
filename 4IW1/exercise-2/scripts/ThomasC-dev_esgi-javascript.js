7d78f9e01638e15b004a75a7bf8cd287539bda9a
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 17:59:29 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/ThomasC-dev_esgi-javascript.js
@@ -0,0 +1,26 @@
+function type_check_v1(vari, type) {
+  switch(typeof vari) {
+    case 'object':
+      if (Array.isArray(vari)) return type === "array";
+      if (vari === null) return type === "null";
+    default:
+      return vari === type;
+  }
+}
+
+function type_check_v2(vari, conf) {
+  for(key in conf) {
+    switch(key) {
+      case 'type':
+        if(!type_check_v1(vari, conf[key])) return false;
+      case 'value':
+        //...
+      case 'enum':
+        //...
+    }
+  }
+  return true;
+}
+
+module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
