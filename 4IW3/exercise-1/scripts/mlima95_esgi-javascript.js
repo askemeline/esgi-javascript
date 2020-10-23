b487844a4f765e5c451f1714e6a27f6b4809145e
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 14:46:50 +0200

    update reports

--- 4IW3/exercise-1/scripts/mlima95_esgi-javascript.js
+++ 4IW3/exercise-1/scripts/mlima95_esgi-javascript.js
@@ -53,6 +53,33 @@ function yoda(string){
     
 }
 
+function vig(string, code) {
+    if (typeof string !== "string") return "";
+    if (string.length === 0) return string;
+  
+    while (code.length < string.length) {
+      code += code;
+    }
+    code = code.substr(0, string.length);
+    let codeIndex = 0;
+  
+    return string
+      .split("")
+      .map((letter, index) => {
+        letter = letter.toLowerCase();
+        const aCode = "a".charCodeAt(0);
+        const letterNumber = letter.charCodeAt(0) - aCode;
+  
+        if (letterNumber < 0 || letterNumber > 25) return letter;
+  
+        const codeNumber = code.charCodeAt(codeIndex) - aCode;
+        codeIndex++;
+  
+        return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+      })
+      .join("");
+  }
+
 
 
 module.exports.ucfirst = ucfirst;
@@ -62,4 +89,5 @@ module.exports.snake_case = snake_case;
 module.exports.leet = leet;
 module.exports.verlan = verlan;
 module.exports.yoda = yoda;
+module.exports.vig = vig;
 module.exports.prop_access = prop_access;
