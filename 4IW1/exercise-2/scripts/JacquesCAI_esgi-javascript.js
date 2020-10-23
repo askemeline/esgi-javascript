6e0a35d9c182e39c0bbd9811b0e413bab616ba5d
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 14:38:57 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/JacquesCAI_esgi-javascript.js
@@ -0,0 +1,64 @@
+function type_check_v1(val, type) {
+  switch (typeof val) {
+    case "object":
+      if (type === "array") return Array.isArray(val);
+      if (type === "null") return val === null;
+      return val != null && !Array.isArray(val);
+    default:
+      return typeof val === type;
+  }
+}
+
+function type_check_v2(object, arr) {
+  checker = true;
+  Object.keys(arr).forEach(function (element) {
+    if (!checker) return checker;
+
+    switch (element) {
+      case "type":
+        checker = type_check_v1(object, arr["type"]);
+        break;
+      case "value":
+        checker = JSON.stringify(object) === JSON.stringify(arr["value"]);
+        break;
+      case "enum":
+        let found = false;
+        enum_loop: {
+          for (value of arr.enum) {
+            if (type_check_v1(object, "array")) {
+              for (el of object) {
+                if (type_check_v2(el, { value: value })) {
+                  found = true;
+                  break enum_loop;
+                }
+              }
+            } else if (type_check_v2(object, { value: value })) {
+              found = true;
+              break;
+            }
+          }
+        }
+        if (!found) return false;
+        break;
+    }
+  });
+  return checker;
+}
+
+function type_check(arg, types) {
+  let isChecked = type_check_v2(arg, types);
+  if (!types.properties) return isChecked;
+  for (const typeKey in types.properties) {
+    isChecked = type_check(
+      type_check_v1(arg, "object") ? arg[typeKey] : arg,
+      types.properties[typeKey]
+    );
+    if (!isChecked) break;
+  }
+  return isChecked;
+}
+
+
+module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
+module.exports.type_check = type_check;
---
+++ 4IW3/exercise-1/scripts/saintmathieu_esgi-javascript.js
@@ -0,0 +1,62 @@
+function ucfirst(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.charAt(0).toUpperCase() + chaine.slice(1);
+}
+
+
+
+function capitalize(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+       let splitStr = chaine.toLowerCase().split(' ');
+   for (let i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(' '); 
+}
+
+
+function camelCase(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    let splitStr = chaine.toLowerCase().split(' ');
+   for (let i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(''); 
+
+}
+
+
+function snake_case(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    var splitStr = chaine.toLowerCase().split(' ');
+    return splitStr.join('_'); 
+}
+
+
+function leet(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    let crypt = chaine.replace(/[aA]/g, '4').replace(/[eE]/g, '3').replace(/[iI]/g, '1').replace(/[oO]/g, '0').replace(/[uU]/g, '(_)').replace(/[yY]/g, '7');
+    return crypt;
+}
+
+
+
+let prairie = { 
+    animal: {
+        type: {
+            name : 'chien'
+        }
+    }
+}
+
+function propAccess(obj, type) {
+
+}
+
+propAccess(prairie, "animal.type.name");
+
+module.exports.ucfirst = ucfirst;
+module.exports.capitalize = capitalize;
+module.exports.camelCase = camelCase;
+module.exports.snake_case = snake_case;
+module.exports.leet = leet;
