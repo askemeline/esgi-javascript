ed0e0996874b34515245b3d46ebf33c282b17515
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:39:36 +0200

    update reports

--- 4IW1/exercise-2/scripts/12Wass_esgi-javascript.js
+++ 4IW1/exercise-2/scripts/12Wass_esgi-javascript.js
@@ -1,28 +1,33 @@
 function type_check_v1(arg, type){
     if (typeof(arg) === "object"){
-        if (Array.isArray(arg)){
-            if (type === "array"){
-                return true;
-            }
-            return false; 
-        }
+        if (type === "array") return Array.isArray(arg); 
+        if (type === "null") return val === null;
+        if (type === "function") return val instanceof Function; 
     }
-        if (typeof(arg) !== type) {
-            return false;
-        }
-        return true; 
+    return typeof val === type; 
  }
 
 function type_check_v2(arg, conf){
+    if (typeof(arg) === "object"){
+        arg.forEach(a => {
+            if (typeof(conf) === "enum"){
+                conf.forEach(en => {
+                    if (en === arg){
+                        return true;
+                    }
+                });
+            }
+        });
+    }
     if (typeof(arg) == conf.type && arg == conf.value){
         return true;
     }
     return false; 
 }
 
-console.log(type_check_v1({"1": "1", "2": "2", "3": "3"}, "object")); // JSON 
-console.log(type_check_v1(["1", "2", "3"], "array")); // Array
+console.log(type_check_v1(undefined, "undefined")); // JSON 
 
+console.log(type_check_v2()); 
 
 module.exports.type_check_v1 = type_check_v1;
 module.exports.type_check_v2 = type_check_v2;
