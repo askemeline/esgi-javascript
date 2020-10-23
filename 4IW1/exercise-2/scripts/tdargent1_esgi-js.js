1debf725ab3413df2fb6efbe2fd8faaf340ec594
Karl MARQUES <kmarques@vetixy.com>
2020-10-23 18:29:45 +0200

    update reports

---
+++ 4IW1/exercise-2/scripts/tdargent1_esgi-js.js
@@ -0,0 +1,26 @@
+function type_check_v1(val, type) {
+    switch (typeof val) {
+        case "object":
+            if (type === "array") {
+                return Array.isArray(val);
+            }
+            if (type === "null") {
+                return val === null;
+            }
+
+            return val != null && ! Array.isArray(val);
+        default:
+            return typeof val === type;
+    }
+}
+
+/* Vérification pour la fonction type_check_v1 */
+console.log(type_check_v1(1, "number"));
+console.log(type_check_v1(1, "string"));
+console.log(type_check_v1("2", "string"));
+console.log(type_check_v1([1, 2], "array"));
+
+/* Vérification pour la fonction type_check_v2 */
+
+module.exports.type_check_v1 = type_check_v1;
+module.exports.type_check_v2 = type_check_v2;
