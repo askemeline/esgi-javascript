7a2a867e768815bc6bbfed70b4e79170edf505cd
Karl MARQUES <kmarques@vetixy.com>
2020-12-15 15:28:42 +0100

    [4IW2] promise

--- 4IW2/exercise-6/promise-fetch.js
+++ 4IW2/exercise-6/promise-fetch.js
@@ -16,22 +16,17 @@ const getCourses = function () {
 };
 
 const mapping = function (resolve) {
-  const timeout = randomIntFromInterval(4, 8);
-
   Promise.all([getStudents(), getCourses()]).then((results) => {
-    setTimeout(() => {
-      console.log(`mapping:${timeout}`);
-      const students = results[0],
-        courses = results[1];
-      resolve(
-        students.map((student) => {
-          student.cours = student.cours.map((id) =>
-            courses.find((cours) => cours.id === id)
-          );
-          return student;
-        })
-      );
-    }, timeout);
+    const students = results[0],
+      courses = results[1];
+    resolve(
+      students.map((student) => {
+        student.cours = student.cours.map((id) =>
+          courses.find((cours) => cours.id === id)
+        );
+        return student;
+      })
+    );
   });
 };
 
