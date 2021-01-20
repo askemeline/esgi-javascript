ddfbf7853a76a103a38ef3f7e78e910ace3af510
Karl MARQUES <kmarques@vetixy.com>
2021-01-20 15:11:20 +0100

    [Ex] project

--- 4IW1/project/main.js
+++ 4IW1/project/main.js
@@ -28,12 +28,111 @@ let React = {
   },
 };
 
+class HelloWorld extends Component {
+  propTypes = {
+    name: { type: "string", enum: ["world", "you", "me"] },
+  };
+
+  render() {
+    return React.createElement("div", { toWhat: { name: this.props.name } }, [
+      "Hello {{toWhat.name}}",
+    ]);
+  }
+}
+
+class Component {
+  state = {};
+  constructor(props) {
+    this.props = props;
+  }
+  display(props) {
+    //...
+    return this.render();
+  }
+  render() {}
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: props.defaultValue || 0,
+    };
+  }
+
+  propTypes = {
+    defaultValue: { type: "number" },
+  };
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "button",
+        { onClick: () => this.setState({ counter: this.state.counter + 1 }) },
+        ["Add"]
+      ),
+      React.createElement("span", { title: this.state.counter }, ["{{title}}"]),
+    ]);
+  }
+}
+
+class Router extends Component {
+  state = {
+    path: "/home",
+  };
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "button",
+        { onClick: () => this.setState({ path: "/home" }) },
+        ["Home"]
+      ),
+      React.createElement(
+        "button",
+        { onClick: () => this.setState({ path: "/about" }) },
+        ["About"]
+      ),
+      path === "/home" && React.createElement(Home),
+      path === "/about" && React.createElement(About),
+    ]);
+  }
+}
+
 ReactDOM.render(
   React.createElement("div", { toWhat: { name: "World" } }, [
     "Hello {{toWhat.name}}",
-    React.createElement("div", { toWhat: { name: "World" } }, [
-      "Hello {{toWhat.name}}",
-    ]),
+    React.createElement(HelloWorld, { name: "world" }),
+    React.createElement(Counter, { defaultValue: 10 }),
+    React.createElement(Counter, { defaultValue: 0 }),
   ]),
   document.getElementById("root")
 );
+
+//<=>
+//<div>
+//  Hello World
+//  <div>
+//    Hello World
+//  </div>
+//  <div>
+//    <button>Add</button>
+//    <span>10</span>
+//  </div>
+//  <div>
+//  <button>Add</button>
+//    <span>0</span>
+//  </div>
+//</div>
+
+//I] Pros : generation, Cons: Update
+//  React.createElement => DomElement
+//  Component.render => DomElement
+//  ReactDOM.render => rootElement.appendChild(DomElement);
+//
+//II] Pros: Update, Cons: generation
+//  React.createElement => Object
+//  Component.render => Object
+//  ReactDOM.render =>
+//    1) Object => DomElement
+//    2) rootElement.appendChild(DomElement);
